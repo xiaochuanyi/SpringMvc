a3bff7c6d121609d803e02f74dc6f94c7cd0f79c
xiaochuanyi <1104849478@qq.com>
2018-04-19 14:59:54 +0800

    入门程序

---
+++ src/bean/Iteam.java
@@ -0,0 +1,51 @@
+package bean;
+
+import java.util.Date;
+
+public class Iteam {
+	private int id;
+	private String name;
+	private Float price;
+	private Date createtime;
+	private String detail;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Float getPrice() {
+		return price;
+	}
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+	public Date getCreatetime() {
+		return createtime;
+	}
+	public void setCreatetime(Date createtime) {
+		this.createtime = createtime;
+	}
+	public String getDetail() {
+		return detail;
+	}
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+	public Iteam(int id, String name, Float price, Date createtime, String detail) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.createtime = createtime;
+		this.detail = detail;
+	}
+	
+
+}
---
+++ src/controller/ItemController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import bean.Iteam;
+
+/*
+ * 商品管理
+ */
+@Controller
+public class ItemController {
+	@RequestMapping(value="/item/itemlist.action")
+	public ModelAndView itemList(){
+		List<Iteam> list = new ArrayList<Iteam>();
+		list.add(new Iteam(1,"魅族note6",1099f,new Date(),"便宜"));
+		list.add(new Iteam(1, "1华为 荣耀8", 2399f, new Date(), "质量好！1"));
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("itemList", list);//itemList是jsp中的，即把list值赋给itemList
+		mav.setViewName("/WEB-INF/jsp/itemList.jsp");
+		return mav;
+	}
+
+}
